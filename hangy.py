524dd21d53ce63f1eb4718d9d28c1c32b3c4d460
David Fazakas <david.fazakas@student.unitbv.ro>
2024-03-25 00:52:22 +0200

    updated to work properly

--- hangy.py
+++ hangy.py
@@ -1,86 +1,97 @@
 import random
 import requests
+import csv
 
 hangman_steps = [
-"""
-------
-| |
-|
-|
-|
-|
-|
-|___
-""",
-"""
-------
-| |
-| O
-|
-|
-|
-|
-|___
-""",
-"""
-------
-| |
-| O
-| |
-|
-|
-|
-|___
-""",
-"""
-------
-| |
-| O
-| /|
-|
-|
-|
-|___
-""",
-"""
-------
-| |
-| O
-| /|\\
-|
-|
-|
-|___
-""",
-"""
-------
-| |
-| O
-| /|\\
-| /
-|
-|
-|___
-""",
-"""
-------
-| |
-| O
-| /|\\
-| / \\
-|
-|
-|___
-"""
+    """
+    ------
+    | |
+    |
+    |
+    |
+    |
+    |
+    |___
+    """,
+    """
+    ------
+    | |
+    | O
+    |
+    |
+    |
+    |
+    |___
+    """,
+    """
+    ------
+    | |
+    | O
+    | |
+    |
+    |
+    |
+    |___
+    """,
+    """
+    ------
+    | |
+    | O
+    | /|
+    |
+    |
+    |
+    |___
+    """,
+    """
+    ------
+    | |
+    | O
+    | /|\\
+    |
+    |
+    |
+    |___
+    """,
+    """
+    ------
+    | |
+    | O
+    | /|\\
+    | /
+    |
+    |
+    |___
+    """,
+    """
+    ------
+    | |
+    | O
+    | /|\\
+    | / \\
+    |
+    |
+    |___
+    """
 ]
 
 
-
 def game_setup():
-    #secret_words = ["hi", "horse", "love", "coffee", "joke", "table", "Hanna", "Fikusz", "Muki", "sound"]
-    response = requests.get('https://random-word-api.herokuapp.com/word')
-    letters = list(response.text)
-    word = letters[2:len(letters) - 2]
+    # secret_words = ["hi", "horse", "love", "coffee", "joke", "table", "Hanna", "Fikusz", "Muki", "sound"]
+    while True:
+        try:
+            response = requests.get('https://random-word-api.herokuapp.com/word', timeout=5)
+        except requests.exceptions.RequestException:
+            with open('names.csv', newline='') as names_csv_file:
+                csv_reader = csv.reader(names_csv_file)
+                next(csv_reader)
+                word = (random.choice([line[0] for line in csv_reader]))
+                break
+        else:
+            letters = list(response.text)
+            word = letters[2:len(letters) - 2]
+            break
+
     word2 = list(word)
     for x in range(len(word)):
         word2[x] = '_'
@@ -88,12 +99,12 @@ def game_setup():
         print("_", end="")
     return word, word2
 
+
 def input_function():
-    x = False
-    while x != True:
+
+    while True:
         letter = input("\nEnter a letter: ")
-        if len(letter) == 1 and letter.isalpha() == True:
-            x = True
+        if len(letter) == 1 and letter.isalpha() is True:
             return letter
         else:
             print("this is not a letter")
@@ -109,12 +120,11 @@ def correct_guess(letter, word, word2):
         return word2
 
 
-
-
 def incorrect_guess(letter, word, attempt):
     if letter not in word:
         print(hangman_steps[7 - attempt])
         return True
+
 
 def winning_function(word, word2):
     if ''.join(word2) == word:
@@ -131,13 +141,14 @@ def loosing_function(attempt):
 def game_over(word):
     print("The secret word is: ", ''.join(word))
 
+
 def main():
     attempt = 7
     word, word2 = game_setup()
     while True:
         letter = input_function()
         word2 = correct_guess(letter, word, word2)
-        if incorrect_guess(letter, word, attempt) == True:
+        if incorrect_guess(letter, word, attempt) is True:
             attempt = attempt - 1
         if winning_function(word, word2) is True:
             break
@@ -147,6 +158,6 @@ def main():
     game_over(word)
 
 
-
 main()
+
 
